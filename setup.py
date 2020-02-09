8480d93980af13a8b2856f94665c5237a4a169e8
witrdotnet <witr.net@gmail.com>
2020-02-09 11:54:54 +0100

    Release 1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="wAsciiArt",
-    version="1.0",
+    version="1.1",
     packages=find_packages("src"),
     package_dir={"": "src"},
     author="witrdotnet",
